b24a6a00b121fb31c925cdbb67033bd16b48322c
vladimir_feshenko <vladimir0feshenko@gmail.com>
2023-11-21 18:09:46 +0300

    init

---
+++ src/main/java/ru/clevertec/course/spring/SpringVideoHostingApplication.java
@@ -0,0 +1,13 @@
+package ru.clevertec.course.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVideoHostingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringVideoHostingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/clevertec/course/spring/SpringVideoHostingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.clevertec.course.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVideoHostingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
